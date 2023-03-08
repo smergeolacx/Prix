7659733017612c2b49af5c79138b641ccd538d74
Anish Pramod <anish.solleti@gmail.com>
2023-03-09 02:39:22 +0530

    modified scanner class with basic grammer

--- Scanner.java
+++ Scanner.java
@@ -59,11 +59,98 @@ class Scanner {
 				break;
 			case '\n': line++; break;
 			case '"': string(); break;
-			default: Lox.error(line, "Unexpected character."); break;
+			default:
+				if (isDigit(c)) {
+					number();
+				}
+				else if(isAlpha(c)) {
+					identifier();
+				}
+				else { 
+					Lox.error(line, "Unexpected character.");
+				}
+				break;
    		}
 	}
 
+  }
+
+	 private static final Map<String, TokenType> keywords;
+
+  	static {
+    		keywords = new HashMap<>();
+    		keywords.put("and",    AND);
+    		keywords.put("class",  CLASS);
+    		keywords.put("else",   ELSE);
+    		keywords.put("false",  FALSE);
+    		keywords.put("for",    FOR);
+    		keywords.put("fun",    FUN);
+    		keywords.put("if",     IF);
+    		keywords.put("nil",    NIL);
+    		keywords.put("or",     OR);
+    		keywords.put("print",  PRINT);
+    		keywords.put("return", RETURN);
+    		keywords.put("super",  SUPER);
+    		keywords.put("this",   THIS);
+    		keywords.put("true",   TRUE);
+    		keywords.put("var",    VAR);
+    		keywords.put("while",  WHILE);
+  	}
+
+	 private void identifier() {
+    		while (isAlphaNumeric(peek())) advance();
+		String text = source.substring(start, current);
+    		TokenType type = keywords.get(text);
+    		if (type == null) type = IDENTIFIER;
+    		addToken(type);
+  	}
+
+  	private boolean isAlpha(char c) {
+    		return (c >= 'a' && c <= 'z') ||
+           	(c >= 'A' && c <= 'Z') ||
+        	c == '_';
+  	}
+
+  	private boolean isAlphaNumeric(char c) {
+    		return isAlpha(c) || isDigit(c);
+  	}
+
+
+	private void number() {
+		while(isDigit(peek())) advance();
+		if (peek() == '.' && isDigit(peekNext())) {
+			advance();
+			while (isDigit(peek())) advance();
+		}
+
+		addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+	}
+
+	private void peekNext() {
+		if(current+1 >= source.length()) return '\0';
+		return source.charAt(current + 1);
+	}
+
+	private void isDigit(char c) {
+		return c >= '0' && c <= '9'
+	}
+
 	private void string() {
+		while(peek() != isAtEnd()){
+			if(peek() == '\n') line++;
+			advance();
+		}
+
+		if (isAtEnd()){
+			Lox.error(line, "unterminated line. ");
+			return;
+		}
+
+		advance();
+
+		String value = source.substring(start + 1, current - 1);
+		addToken(STRING, value);
+
 	}
 
 	private boolean match(char expected) {
